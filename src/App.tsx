1850cd171ab004e1d2366d61099ac86537a2b5d4
SkyG0D <silvanosilvino@hotmail.com>
2022-07-21 23:42:34 -0300

    feat: add EventsCalendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { Center } from '@chakra-ui/react';
+import { addDays, subDays } from 'date-fns';
 
 import { EventsCalendar } from './components/EventsCalendar';
 
 export function App() {
   return (
     <Center>
-      <EventsCalendar />
+      <EventsCalendar
+        onClickDay={console.log}
+        events={[
+          { id: '1', title: 'Festa Infantil', date: subDays(new Date(), 6) },
+          { id: '2', title: 'Festa Infantil', date: subDays(new Date(), 9) },
+          { id: '3', title: 'Festa Infantil', date: addDays(new Date(), 1) },
+          { id: '4', title: 'Festa Infantil', date: addDays(new Date(), 2) },
+          { id: '5', title: 'Festa Infantil', date: addDays(new Date(), 2) },
+          { id: '6', title: 'Festa Infantil', date: addDays(new Date(), 2) },
+          { id: '7', title: 'Festa Infantil', date: addDays(new Date(), 2) },
+          { id: '8', title: 'Festa Infantil', date: addDays(new Date(), 2) },
+        ]}
+      />
     </Center>
   );
 }
--- src/components/EventsCalendar/index.tsx
+++ src/components/EventsCalendar/index.tsx
@@ -1,27 +1,77 @@
-import Calendar from 'react-calendar';
-import { format } from 'date-fns';
+import Calendar, { DateCallback } from 'react-calendar';
+import { format, isSameDay, isSameMonth, isSameYear } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import { Text } from '@chakra-ui/react';
+import { Box, Center, Icon, Text } from '@chakra-ui/react';
+import { BsPlusLg } from 'react-icons/bs';
 
-import './styles.scss';
 import { titleString } from '../../utils/titleString';
 
-export function EventsCalendar() {
+import './styles.scss';
+
+interface Event {
+  id: string;
+  title: string;
+  date: Date;
+}
+
+interface EventsCalendarProps {
+  events: Event[];
+  onClickDay?: DateCallback;
+}
+
+const MAX_EVENTS_DISPLAY = 3;
+
+function isSameDate(leftDate: Date, rightDate: Date) {
+  return (
+    isSameDay(leftDate, rightDate) &&
+    isSameMonth(leftDate, rightDate) &&
+    isSameYear(leftDate, rightDate)
+  );
+}
+
+export function EventsCalendar({ events, onClickDay }: EventsCalendarProps) {
   function formatShortWeekday(_: string, date: Date) {
     return format(date, 'EEEE', { locale: ptBR })[0];
   }
 
+  function navigationLabel({ date }: { date: Date }) {
+    return (
+      <Text fontWeight="bold" fontSize="2xl">
+        {titleString(format(date, 'MMMM yyyy', { locale: ptBR }))}
+      </Text>
+    );
+  }
+
+  function titeContent({ date }: { date: Date }) {
+    const eventsInDay = events.filter((event) => isSameDate(event.date, date));
+
+    if (eventsInDay.length === 0) {
+      return <></>;
+    }
+
+    const eventsSymbols = eventsInDay
+      .slice(0, MAX_EVENTS_DISPLAY)
+      .map(({ id }) => (
+        <Box key={id} w="2" h="2" rounded="full" bg="orange.300" />
+      ));
+
+    return (
+      <Center mt="1" as={Center} flexWrap="wrap" gap="1px">
+        {eventsSymbols}
+        {eventsInDay.length > MAX_EVENTS_DISPLAY && (
+          <Icon as={BsPlusLg} w="2" h="2" color="orange.300" />
+        )}
+      </Center>
+    );
+  }
+
   return (
     <Calendar
-      navigationLabel={({ date }) => {
-        return (
-          <Text fontWeight="bold" fontSize="2xl">
-            {titleString(format(date, 'MMMM yyyy', { locale: ptBR }))}
-          </Text>
-        );
-      }}
+      navigationLabel={navigationLabel}
       showNeighboringMonth={false}
       formatShortWeekday={formatShortWeekday}
+      tileContent={titeContent}
+      onClickDay={onClickDay}
     />
   );
 }
